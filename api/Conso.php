c56b6185d188fe361eb4e129fcbaf41f716351cc
VMS <113035797+Salomon-Vms@users.noreply.github.com>
2024-06-27 14:39:06 +0200

    Delete api directory

--- api/Conso.php
+++
@@ -1,9 +0,0 @@
-$apiUrl = 'http://localhost/api/creation.php';
-$responseData = file_get_contents($apiUrl);
-
-if ($responseData) {
-    $decodedData = json_decode($responseData, true); 
-    print_r($decodedData); 
-} else {
-    echo "Erreur lors de la récupération des données de l'API";
-}
